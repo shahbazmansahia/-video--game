923102a08b2fc3aed779ad755ca80f2d972094f9
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2022-07-14 16:22:53 -0700

    Added vertical-forward movement
    Added the vertical movement based on vertical axis input via the InputMapper. The speed set is good for testing. TO DO: Develop the mentioned stubs, add sound, add vibrations, add collision events, add horizontal movement, test without camera on.

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public float horizontalInput;
+    public float forwardInput;
+    public float speed = 0.5f;
+    public float walkForce = 0.5f;
+
+    private Rigidbody playerRb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // gets the rigidbody object attached to player gameObject
+        playerRb = GetComponent<Rigidbody>();   
+    }
+
+    private void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        //playerRb.AddRelativeForce(Vector3.forward * forwardInput * walkForce);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * forwardInput);
+
+        //transform.(Vector3.forward, Time.deltaTime * speed * forwardInput);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // TO DO: Add sound and optional rumble fx as you walk
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // TO DO: Add collision interaction with walls and objects
     }
 }
